1005d5df650c6ac22a34b9869dbc2e25dd017da4
pradhan-05p <pradhanprateek2007@gmail.com>
2026-07-23 16:06:52 +0530

    changes in attendance voice

--- src/components/attendance_voice_dialouge.py
+++ src/components/attendance_voice_dialouge.py
@@ -18,13 +18,18 @@ def attendance_result_dialouge_voice(df,logs):
             try:
                 crete_attendance_logs(logs)
                 st.toast("Attendance logs saved successfully!",icon="✅")
-                time.sleep(1)
                 st.session_state.attendance_image = []
                 st.session_state.voice_attendance_results = None
+                time.sleep(1)
                 st.rerun()
             except Exception as e:
                 st.error(f"An error occurred while saving attendance: {e}", icon="❌")
 
+
 @st.dialog(title="Attendance Reports (Voice)")
 def attendance_result_dialouge(df,logs):
-    attendance_result_dialouge_voice(df,logs)
+    if st.session_state.get("voice_attendance_results") is not None:
+        attendance_result_dialouge_voice(
+            df=st.session_state.voice_attendance_results[0],
+            logs=st.session_state.voice_attendance_results[1]
+        )
