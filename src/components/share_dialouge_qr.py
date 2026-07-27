9b58ec1ae920638178304911741b9e475c84c40e
PRATEEK PRADHAN <pradhanprateek2007@gmail.com>
2026-07-27 15:50:29 +0530

    Update share_dialouge_qr.py
    fixed the url of the qr sharing

--- src/components/share_dialouge_qr.py
+++ src/components/share_dialouge_qr.py
@@ -3,7 +3,7 @@ import segno
 import io
 @st.dialog(title="Share Subject Code")
 def share_sub_dialogue(subject_code, subject_name):
-    app_domain = "snapclass-main-p.streamlit.app"
+    app_domain = "snapclass-main-attend.streamlit.app"
     join_url = f"{app_domain}?join-code={subject_code}"
     
     st.write(f"Scan to join")
